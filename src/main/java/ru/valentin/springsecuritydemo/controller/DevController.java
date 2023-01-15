9061d031652672398c83c2fc03bbd338e091e5fc
valentin.zhuravel <valentin.zhuravel@x5.ru>
2023-01-15 18:36:17 +0300

    fixed immutable collection

--- src/main/java/ru/valentin/springsecuritydemo/controller/DevController.java
+++ src/main/java/ru/valentin/springsecuritydemo/controller/DevController.java
@@ -5,15 +5,18 @@ import ru.valentin.springsecuritydemo.model.Developer;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(value = "/api/v1/developers")
 public class DevController {
   private List<Developer> DEVELOPERS =
-      List.of(
-          new Developer(1L, "Valya", "Zhur"),
-          new Developer(2L, "Alex", "Gorshkov"),
-          new Developer(3L, "Alex", "Kruglov"));
+      Stream.of(
+              new Developer(1L, "Valya", "Zhur"),
+              new Developer(2L, "Alex", "Gorshkov"),
+              new Developer(3L, "Alex", "Kruglov"))
+          .collect(Collectors.toList());
 
   @GetMapping
   public List<Developer> getAll() {
